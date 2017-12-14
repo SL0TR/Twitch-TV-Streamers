4a8a351419a5f61201a31c6f8134d3ce7e8363ba
Mohaiminislam94 <mohaiminislam94@gmail.com>
2017-12-14 13:26:56 +0600

    added links of API's

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+
+    // https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2
+    // https://wind-bow.glitch.me/twitch-api/users/freecodecamp
+
     var api = "https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2";
 
     $.ajax({    
