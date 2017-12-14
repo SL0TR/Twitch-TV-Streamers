8df1d2b7d0afef63d2942af8899a775cce8487f8
SL0TR <yomaimajs@gmail.com>
2017-12-14 21:51:38 +0600

    minor fixes

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 $(document).ready(function(){
-
-
-    // https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2
-    // https://wind-bow.glitch.me/twitch-api/users/freecodecamp
-
-    var api = "https://wind-bow.glitch.me/twitch-api/";
-    // var type = ["users", "streams"];
-    // var streamer = ["ESL_SC2", "freecodecamp", "riotgames" ];
-
-
-
-    function fetchApiData (type, streamer) {
-        $.ajax({    
-            url: (api + type + '/' + streamer)  ,
-            type: 'GET',
-            dataType: 'jsonp',
-            success: function(a) {
-                
-            var parsedJsonData = a;
-            console.log(parsedJsonData);
-            
-
-
-            },
-            error: function() { console.log('Failed!'); },
-        });
-
-
-    };
-
-    // RIOTGAMES
-    function riotGames() {
-        fetchApiData("users", "riotgames");  
-        console.log("wtf");
-        streamerLogo = parsedJsonData.logo;
-        $(".riotgames-logo").attr("src", streamerLogo);
-
-    }
-
-    riotGames();
-
-     
-
     
-
-});
+    
+        // https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2
+        // https://wind-bow.glitch.me/twitch-api/users/freecodecamp
+    
+        var api = "https://wind-bow.glitch.me/twitch-api/";
+        // var type = ["users", "streams"];
+        // var streamer = ["ESL_SC2", "freecodecamp", "riotgames" ];
+    
+    
+    
+        function fetchApiData (type, streamer) {
+            $.ajax({    
+                url: (api + type + '/' + streamer)  ,
+                type: 'GET',
+                dataType: 'jsonp',
+                success: function(a) {
+                    
+                var parsedJsonData = a;
+                console.log(parsedJsonData);
+                streamerLogo = parsedJsonData.logo;
+                riotGames();
+                },
+                error: function() { console.log('Failed!'); },
+            });
+    
+    
+        };
+    
+        // RIOTGAMES
+        function riotGames() {
+             
+    
+            $(".riotgames-logo").attr("src", streamerLogo);
+    
+        }
+    
+    
+    
+        fetchApiData("users", "riotgames");   
+    
+        
+    
+    });
