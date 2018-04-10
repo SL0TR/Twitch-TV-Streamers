d7f0e3de7730bc0f0c6c7585f10326a7094878fe
SL0TR <mohaiminislam94@gmail.com>
2018-04-10 15:35:37 +0600

    make api variable for re-usablity

---
+++ twitch.tv-streamers/src/api.js
@@ -0,0 +1,11 @@
+//  https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2
+//  https://wind-bow.glitch.me/twitch-api/users/freecodecamp
+//  https://wind-bow.glitch.me/twitch-api/
+
+export const root = 'https://wind-bow.glitch.me/'
+
+export const api = root + 'twitch-api/'
+
+export const users = api + 'users/'
+
+export const streams = api + 'steams/'
--- twitch.tv-streamers/src/pages/index.vue
+++ twitch.tv-streamers/src/pages/index.vue
@@ -1,43 +1,14 @@
 <template>
   <q-page>
     <section class="mainPage">
-      <div class="header">
+      <div class="header-title">
         <h2>Twitch Tv Streamers</h2>
       </div>
       <div class="monitorContainer">
         <img class="monitor" src="https://www.directpackages.com/assets/seo-c/images/devices/tv.png" alt="monitor">
         <div class="monitorContent">
-          <div class="monitorNav">
-            <div class="all stream-status active">
-              <i class="ion-navicon-round"></i>
-              <p>All</p>
-            </div>
-            <div class="online stream-status">
-              <i class="ion-ios-circle-filled"></i>
-              <p>Online</p>
-            </div>
-            <div class="offline stream-status">
-              <i class="ion-ios-circle-outline"></i>
-              <p>Offline</p>
-            </div>
-          </div>
-          <div class="monitorSubContent">
-            <div class="stream-card">
-              <p class="streamer-name"></p>
-              <img class="streamer-logo esl-logo"  alt="logo">
-              <p class="status"></p>
-            </div>
-            <div class="stream-card">
-              <p class="streamer-name"></p>
-              <img class="streamer-logo riotgames-logo" salt="logo">
-              <p class="status"></p>
-            </div>
-            <div class="stream-card">
-              <p class="streamer-name"></p>
-              <img  class="streamer-logo fcc-logo"  alt="logo">
-              <p class="status"></p>
-            </div>
-          </div>
+          <monitorNav/>
+          <monitorSubContent/>
         </div>
       </div>
     </section>
@@ -62,17 +33,12 @@
   background: linear-gradient(to bottom, #351A6B, #6445A2);
 }
 
-.header {
+.header-title {
   text-align: center;
   width: 100%;
   position: relative;
-  bottom: 4rem;
+  bottom: 3rem;
   color: #eee;
-}
-
-.twitch-logo {
-  height: 3.5em;
-  width: 6em;
 }
 
 .monitorContainer {
@@ -87,8 +53,6 @@
 }
 
 .monitorContent {
-  /* background: linear-gradient(to right, #6441a5, #2a0845); */
-  /* background: rgba(124, 90, 174, .8); */
   background: #81ecec;
   position: absolute;
   height: calc(100% - 6em);
@@ -97,110 +61,21 @@
   left: 0.3em;
   display: flex;
 }
-
-.monitorNav {
-  flex: 1;
-  background: rgba(100, 69, 162, 8);
-  display: flex;
-  flex-direction: column;
-}
-
-.monitorNav .stream-status {
-  margin: 1em 0.5em;
-  height: 25%;
-  border-radius: 0.5em;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  transition: 0.25s;
-}
-
-.stream-status:hover,
-.active {
-  background: rgba(0, 0, 0, 0.3);
-  cursor: pointer;
-}
-
-.stream-status p {
-  color: #eee;
-  font-size: 1em;
-}
-
-.stream-status i {
-  color: #eee;
-  font-size: 2em;
-}
-
-.monitorSubContent {
-  flex: 10;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-
-.stream-card {
-  height: 80%;
-  width: 25%;
-  background: #000;
-  border-radius: 0.5em;
-  box-shadow: 0px 10px 11px -6px rgba(0, 0, 0, 0.75);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  transition: 0.2s;
-  position: relative;
-  overflow: hidden;
-}
-
-.stream-card:hover {
-  transform: scale(1.02);
-  box-shadow: 0px 10px 11px -2px rgba(0, 0, 0, 0.55);
-  cursor: pointer;
-}
-
-.stream-card p {
-  font-size: 2.2em;
-  font-weight: bold;
-  z-index: 1;
-  color: #eee;
-}
-
-.stream-card .streamer-logo {
-  width: 100%;
-  height: 100%;
-  border-radius: 0.2em;
-  position: absolute;
-  z-index: 0;
-  opacity: 0.3;
-}
 </style>
 
 <script>
-//  https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2
-//  https://wind-bow.glitch.me/twitch-api/users/freecodecamp
-//  https://wind-bow.glitch.me/twitch-api/
+import monitorNav from '../components/monitor-nav'
+import monitorSubContent from '../components/monitorSubContent'
+
 export default {
   name: 'PageIndex',
   data () {
     return {
-      users: {}
     }
   },
-  methods: {
-    getTwitchApiData () {
-      this.$axios.get('https://wind-bow.glitch.me/twitch-api/users/freecodecamp')
-        .then(res => {
-          console.log(res)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    }
-  },
-  created () {
-    this.getTwitchApiData()
+  components: {
+    monitorNav,
+    monitorSubContent
   }
 }
 
